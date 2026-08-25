61d26a13d5008bb06a6e74b08d651a8e8161d4b0
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-08-25 11:25:23 +0300

    task

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''async_generator'''
     for _ in range(10):
         await asyncio.sleep(1)
