10337e0bfe0026ad7a3378371f126ea8c7a7b813
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-07 13:48:11 +0300

    task

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        ''' get_hyper_index '''
         data = self.indexed_dataset()
         assert isinstance(index, int) and not index < 0 and index < len(
             data) and isinstance(page_size, int) and page_size > 0
