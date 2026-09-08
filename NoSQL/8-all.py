86c721ae2fca48033086f839c16d3992dee7c894
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 10:56:34 +0300

    task

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-import pymongo
+"""
+Module that lists all documents in a MongoDB collection
+"""
 
 
 def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    Returns an empty list if no document in the collection.
+    """
     return list(mongo_collection.find())
