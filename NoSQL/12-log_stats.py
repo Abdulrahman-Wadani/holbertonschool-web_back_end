26ae2200fb57c74a8bcbc3252cc001a6a52d2fef
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 11:46:53 +0300

    task

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,21 +2,21 @@
 """
 Module that lists all documents in a MongoDB collection
 """
-from pymongo import MongoClient
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
+    clint = MongoClient('mongodb://127.0.0.1:27017')
+    nginc_collection = clint.logs.nginx
 
-clint = MongoClient('mongodb://127.0.0.1:27017')
-nginc_collection = clint.logs.nginx
-
-print(f'{nginc_collection.count_documents({})} logs')
-print(f'Methods:\
-      \n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
-      \n\tmethod POST: {nginc_collection.count_documents({'method': 'POST'})}\
-      \n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
-      \n\tmethod PATCH: {nginc_collection.count_documents(
-    {'method': 'PATCH'})}\
-      \n\tmethod DELETE: {nginc_collection.count_documents(
-        {'method': 'DELETE'})}'
-)
-print(f'{nginc_collection.count_documents(
-    {'method': 'GET', 'path': '/status'})} status check')
+    print(f'{nginc_collection.count_documents({})} logs')
+    print(f'Methods:\
+        \n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
+        \n\tmethod POST: {nginc_collection.count_documents({'method': 'POST'})}\
+        \n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
+        \n\tmethod PATCH: {nginc_collection.count_documents(
+        {'method': 'PATCH'})}\
+        \n\tmethod DELETE: {nginc_collection.count_documents(
+            {'method': 'DELETE'})}'
+    )
+    print(f'{nginc_collection.count_documents(
+        {'method': 'GET', 'path': '/status'})} status check')
