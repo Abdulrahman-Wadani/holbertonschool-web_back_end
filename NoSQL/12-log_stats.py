3872996458bd20eab9cb5f50effe6e340cfff01b
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 11:47:49 +0300

    task

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
     nginc_collection = clint.logs.nginx
 
     print(f'{nginc_collection.count_documents({})} logs')
-    print(f'Methods:\
+    print(f"Methods:\
         \n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
-        \n\tmethod POST: {nginc_collection.count_documents({'method': 'POST'})}\
+        \n\tmethod POST: {nginc_collection.count_documents(
+        {'method': 'POST'})}\
         \n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
         \n\tmethod PATCH: {nginc_collection.count_documents(
-        {'method': 'PATCH'})}\
+            {'method': 'PATCH'})}\
         \n\tmethod DELETE: {nginc_collection.count_documents(
-            {'method': 'DELETE'})}'
+                {'method': 'DELETE'})}"
     )
-    print(f'{nginc_collection.count_documents(
-        {'method': 'GET', 'path': '/status'})} status check')
+    print(f"{nginc_collection.count_documents(
+        {'method': 'GET', 'path': '/status'})} status check")
