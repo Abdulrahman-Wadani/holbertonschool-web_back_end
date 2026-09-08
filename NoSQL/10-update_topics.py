9085ffecaed40f150fcd7bd41d653fed834d7286
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 11:21:34 +0300

    task

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,12 +2,12 @@
 """
 Module that lists all documents in a MongoDB collection
 """
-import pymongo
 
 
-def update_topics(mongo_collection: pymongo, name, topics):
+def update_topics(mongo_collection, name, topics):
     """
     Lists all documents in a collection.
     Returns an empty list if no document in the collection.
     """
-    return mongo_collection.UpdateMany(name, topics)
+    return mongo_collection.update_many(
+        {'name': name}, {'$set': {'topics': topics}})
