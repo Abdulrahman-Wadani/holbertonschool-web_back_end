2760629edd6d132294c7a0cd239295c7d0bc6630
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 11:45:55 +0300

    task

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,8 +8,8 @@ from pymongo import MongoClient
 clint = MongoClient('mongodb://127.0.0.1:27017')
 nginc_collection = clint.logs.nginx
 
-print(f'{nginc_collection.count_documents()} logs')
-print(f'Methods\
+print(f'{nginc_collection.count_documents({})} logs')
+print(f'Methods:\
       \n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
       \n\tmethod POST: {nginc_collection.count_documents({'method': 'POST'})}\
       \n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
@@ -19,4 +19,4 @@ print(f'Methods\
         {'method': 'DELETE'})}'
 )
 print(f'{nginc_collection.count_documents(
-    {'method': 'GET'}, {'path': '/status'})} status check')
+    {'method': 'GET', 'path': '/status'})} status check')
