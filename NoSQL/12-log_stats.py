c752b4915f052c84fa46122aef084060c9487710
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-08 11:49:40 +0300

    task

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 
     print(f'{nginc_collection.count_documents({})} logs')
     print(f"Methods:\
-        \n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
-        \n\tmethod POST: {nginc_collection.count_documents(
+\n\tmethod GET: {nginc_collection.count_documents({'method': 'GET'})}\
+\n\tmethod POST: {nginc_collection.count_documents(
         {'method': 'POST'})}\
-        \n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
-        \n\tmethod PATCH: {nginc_collection.count_documents(
+\n\tmethod PUT: {nginc_collection.count_documents({'method': 'PUT'})}\
+\n\tmethod PATCH: {nginc_collection.count_documents(
             {'method': 'PATCH'})}\
-        \n\tmethod DELETE: {nginc_collection.count_documents(
+\n\tmethod DELETE: {nginc_collection.count_documents(
                 {'method': 'DELETE'})}"
     )
     print(f"{nginc_collection.count_documents(
