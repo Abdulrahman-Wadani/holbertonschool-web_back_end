6cbcaccf719d4bbbe965f0a677ae2f79ba5969c1
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-14 10:30:59 +0300

    task

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+    constructor(size, location) {
+        this._location = location;
+        this._size = size;
+    }
+    [Symbol.toPrimitive](hint) {
+        if (typeof hint === Number) {
+            return this._size;
+        }
+
+        if (typeof hint === String)
+            return this._location;
+    }
+}
