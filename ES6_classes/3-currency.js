0ddf35568ab41dcfddabebd4027c96fe4792bc9d
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-14 09:29:08 +0300

    task

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -6,4 +6,16 @@ export default class Currency {
     displayFullCurrency() {
         return `${this._name} (${this._code})`;
     }
+    get name() {
+        return this._name;
+    }
+    set name(newName) {
+        this._name = newName;
+}
+    get code() {
+        return this._code;
+    }
+    set code(newCode) {
+        this._code = newCode;
+}
 }
