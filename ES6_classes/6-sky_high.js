c939e9002d30b4cdfb8c5b9b4b5de90bc46d7deb
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-14 10:15:33 +0300

    task

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+
+    get floors() {
+        return this._floors;
+    }
+    set floors(newFloors) {
+        this._floors = newFloors;
+    }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the NUMBER_OF_FLOORS floors`;
+    }
+}
