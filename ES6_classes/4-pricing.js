3521beae4227d71ff6ef2cffa5aeebf49591ed05
Abdulrahman-Wadani <abdulrahmanqmb@gmail.com>
2026-09-14 09:40:12 +0300

    task

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -18,7 +18,7 @@ export default class Pricing {
         this._amount = newa;
     }
     displayFullPrice() {
-        return `${this._amount} ${this._currency.code} (${this._currency.name})`
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`
     }
     static convertPrice(amount, conversionRate) {
         return amount * conversionRate;
